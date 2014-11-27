23fe9155024cf046b11e6099590f151d77e615ca
Михаил Флейтман <mikhail.fleytman@2011.auditory.ru>
2014-11-27 03:35:41 +0300

    Удалил комментарий

--- Web4/Web4/app/forms.py
+++ Web4/Web4/app/forms.py
@@ -4,4 +4,3 @@ from wtforms.validators import Required
 
 class SearchForm(Form):
     search = TextField('EIN', validators = [Required()])
-    #remember_me = BooleanField('remember_me', default = False)
