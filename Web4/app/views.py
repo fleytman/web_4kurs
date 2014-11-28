f89519375189520179bd489ea35c83ea8b986993
Михаил Флейтман <mikhail.fleytman@2011.auditory.ru>
2014-11-28 23:46:03 +0300

    Сделал стартовой страницей search.html , index.html пока пустая. Переадресация после поиска теперь тоже на страницу search.html

--- Web4/app/views.py
+++ Web4/app/views.py
@@ -4,18 +4,18 @@ from flask import render_template, flash, redirect
 from app import app
 from forms import SearchForm
 
-@app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html",
         title = 'Home')
 
+@app.route('/')
 @app.route('/search', methods = ['GET', 'POST'])
 def search():
     form = SearchForm()
     if form.validate_on_submit():
         flash("Введённый штрих код: ".decode("utf8") + unicode(form.search.data))
-        return redirect('/index')
+        return redirect('/search')
     return render_template("search.html",
          title="search",
          form = form)
