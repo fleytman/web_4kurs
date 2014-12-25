f889ea5c6acd8071fa9874a57cae22ed6c2d399f
Михаил Флейтман <mikhail.fleytman@2011.auditory.ru>
2014-12-26 00:06:28 +0300

    Revert "Поменял структур"
    This reverts commit e7d3827bdb32ab2a6b51c6d54e559f5cbd62b991.

---
+++ tmp/with_capcha_rus.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from grab import Grab
+
+
+g = Grab()
+g.go('http://gs46.gs1ru.org/GEPIR31/index.jsp?p=gtin&lng=ru')
+g.response.save("out.html")
+
+print g.tree
+print g.tree.xpath("//img")[0]
+
+print g.xpath_text('//img[@id="recaptcha_image"]')
+print g.search(u"recaptcha_image")
+##Устанавливаем в форму поиска значение
+#g.set_input('TabContainerGTIN:TabPanelGTIN:txtRequestGTIN', '4607005930248')
+##Выбираем checkbox
+#g.set_input('TabContainerGTIN:TabPanelGTIN:rblGTIN', 'item')
+#
+#
+##Отправляем запрос
+#g.submit()
+#
+#g.response.save("out1.aspx")
+#
+#print g.search(u'4607005930248')
+#
